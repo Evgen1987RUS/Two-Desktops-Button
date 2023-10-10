bcb815022dfeb8d50e0c00bdc386bfc149968230
evgen1987_rus <isaenko.ea.work@gmail.com>
2023-10-11 01:30:16 +1000

    OOP-like sockets and improvements in Desktop1.py

--- Sockets/SocketClient.py
+++ Sockets/SocketClient.py
@@ -1,15 +1,21 @@
-import socket # TODO: make it so client opens a socket once and is connected until server is terminated
-import time
+import socket # TODO: make it so client opens a socket once and is connected until server is terminated 
+# TODO: this is supposed to be in an infinite loop (unless we exit the desktop app)
+S = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # all are CONSTs
+HOST = socket.gethostname()
+PORT = 65432
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TODO: this is supposed to be in an infinite loop (unless we exit the desktop app)
-host = socket.gethostname()
-port = 65432
-msgToServer = 'Y' # TODO: this should change depending on what button is pressed
+class clientSide:
+    def __init__(self):
+        S.connect((HOST, PORT))
+        print("Connected with", HOST)
 
-s.connect((host, port))
-print("Connected with", host)
+    def SendMsg(msg): # message is taken from button to change the picture on another desktop
+        if msg == "ch": # "ch" is for change
+            S.send(msg.encode())
+            print("Sent the following message to ", HOST, ": ", msg, sep="")
+        else: # msg == "e", "e" is for exit
+            S.send(msg.encode())
+            S.close()     # sends the message to close the desktop and turns this desktop off
+            print("Sent the following message to ", HOST, ": ", msg, sep="" "\n Process terminated")
 
-s.send(msgToServer.encode())
-print("Sent the following message to ", host, ": ", msgToServer, sep="")
-
-# s.close()
+client = clientSide()
--- Sockets/SocketServer.py
+++ Sockets/SocketServer.py
@@ -1,20 +1,28 @@
 import socket # TODO: server should run only when the desktop is booted up, on exit the server should be terminated
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET, SOCK_STREAM - constants, represent adress families and represent the socket types respectively
-host = socket.gethostname()
-port = 65432
+S = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET, SOCK_STREAM - constants, represent adress families and represent the socket types respectively
+HOST = socket.gethostname()
+PORT = 65432
 
-s.bind((host, port)) # binds the socket with the address, requests tuple type
-print("Binded with", host, port)
-s.listen(1) # listens for number of connections
-connection, address = s.accept() # accept a connection
-print("Connected with", address)
-while 1:
-    data = connection.recv(1024) # socket recieves data from client. Only recieves a Y / N signal in this instance
-    if data.decode() == 'N' or data.decode() == "":
-        break
-    print("Message from client:", data.decode())
+class socketServer:        
+    def __init__(self):
+        S.bind((HOST, PORT)) # binds the socket with the address, requests tuple type
+        print("Binded with", HOST, PORT)
+        
+        S.listen(1) # listens until a limit of connections has been reached # FIXME: maybe add another listening faction if a client is terminated, so the server can accept the next one
+        self.con, self.addr = S.accept() # accept a connection
+        print("Connected with", self.addr)
 
-#connection.close() # terminate client connection
-#socket.close() # terminate server
-# both are not needed in this instance as this will be the end of the socket in this project
+    def ActiveServer():
+        while 1:
+            data = server.con.recv(1024).decode() # recieves signal # FIXME: check whether less than 1024 can be supported
+            if data == "e": # if an exit is signaled -> closes connection and server socket
+                server.con.close() 
+                S.close()
+                print("Message from client:", data, "\n Connection and server were terminated")
+                break
+            elif data == "ch":
+                # FIXME: send the signal to 2nd desktop
+                print("Message from client:", data, "\n Changed the background on 2nd desktop")
+        
+server = socketServer()
